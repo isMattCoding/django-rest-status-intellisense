a0c06cfa0bd4ceb29d0bb61aaf8663bb896bc0a6
Matt Clark <clarkm02@gmail.com>
2024-06-21 14:54:28 +0200

    test passing

--- .vscode-test.js
+++ .vscode-test.js
@@ -3,9 +3,9 @@ const { defineConfig } = require("@vscode/test-cli");
 module.exports = defineConfig([
   {
     label: "unitTests",
-    files: "out/test/**/*.test.ts",
+    files: "out/test/**/*.test.js",
     version: "insiders",
-    workspaceFolder: ".vscode-test1",
+    workspaceFolder: "./.vscode-test1",
   },
   // you can specify additional test configurations, too
 ]);
